263c11755c1518ee334a6fdabe223d5dfc049a9d
Jatinder Koranga <jatinderskk8@gmail.com>
2021-12-15 23:32:50 +0530

    first exercise

---
+++ js101_small_problems/easy_1/ex1.js
@@ -0,0 +1,15 @@
+let isOdd = num => {
+  if (num < 0) {
+    num *= -1;
+  }
+
+  if (num % 2 === 1) return 'true';
+  return 'false';
+}
+
+console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
+console.log(isOdd(-8)); // => false
+console.log(isOdd(0)); // => false
+console.log(isOdd(7)); // => true
