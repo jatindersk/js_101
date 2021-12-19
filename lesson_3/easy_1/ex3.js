48727b9060ba7acef19acc8058fc678a6cf1e869
Jatinder Koranga <jatinderskk8@gmail.com>
2021-12-19 23:02:13 +0530

    ex3

---
+++ lesson_3/easy_1/ex3.js
@@ -0,0 +1,7 @@
+let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
+
+console.log(Object.keys(ages).includes('Spot'));
+console.log(Object.keys(ages).includes('Lily'));
+
+// LS
+console.log(ages.hasOwnProperty("Spot"));
