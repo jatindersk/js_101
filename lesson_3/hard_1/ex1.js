a5de18c8b2baad76f4427e03774967205fa3f548
Jatinder Koranga <jatinderskk8@gmail.com>
2021-12-21 02:04:09 +0530

    ex1 hard

---
+++ lesson_3/hard_1/ex1.js
@@ -0,0 +1,15 @@
+function first() {
+  return {
+    prop1: "hi there"
+  };
+}
+
+function second() {
+  return
+  {
+    prop1: "hi there"
+  };
+}
+
+console.log(first()); // { prop: "hi there" }
+console.log(second()); // undefined
