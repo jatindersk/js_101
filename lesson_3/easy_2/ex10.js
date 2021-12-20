4a0c91038f97e08ae6703dd0064965ff2c467ff9
Jatinder Koranga <jatinderskk8@gmail.com>
2021-12-20 19:41:35 +0530

    easy 2 completed

---
+++ lesson_3/easy_2/ex10.js
@@ -0,0 +1,4 @@
+let statement1 = "The Flintstones Rock!";
+console.log(statement1.split('').filter(el => el.includes('t')).length);
+let statement2 = "Easy come, easy go.";
+console.log(statement2.split('').filter(el => el.includes('t')).length);
