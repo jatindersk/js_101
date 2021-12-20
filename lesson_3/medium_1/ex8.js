1fd7fab1716cc94c4649ba46e555fab8a5f2dd76
Jatinder Koranga <jatinderskk8@gmail.com>
2021-12-21 01:57:07 +0530

    ex 8, 9, 10

---
+++ lesson_3/medium_1/ex8.js
@@ -0,0 +1,21 @@
+let munsters = {
+  Herman: { age: 32, gender: "male" },
+  Lily: { age: 30, gender: "female" },
+  Grandpa: { age: 402, gender: "male" },
+  Eddie: { age: 10, gender: "male" },
+  Marilyn: { age: 23, gender: "female" }
+};
+
+function messWithDemographics(demoObject) {
+  Object.values(demoObject).forEach(familyMember => {
+    familyMember["age"] += 42;
+    familyMember["gender"] = "other";
+  });
+}
+
+messWithDemographics(munsters);
+console.log(munsters);
+// Yes, it will get messed up. This is because the callback function's
+// parameter familyMember references the object's nested in munsters object
+// This happens because objects are passed by reference to the
+// location in memory where the actual object is located
