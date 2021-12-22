5380d6151a868ae365f08c057dcff5d41ce1b3ea
Jatinder Koranga <jatinderskk8@gmail.com>
2021-12-23 00:38:38 +0530

    ex3 and 4

---
+++ lesson_4/ex3.js
@@ -0,0 +1,8 @@
+[1, 2, 3].map(num => num * num); // [1, 4, 9]
+
+// The map method returns an array consisting of
+// sqaures of each number in the original array.
+// As the body of arrow function expression implemented
+// has only a simple single line expression
+// it implicitly returns the expression's value without
+// the need of using a return keyword explicitly
---
+++ lesson_4/ex4.js
@@ -0,0 +1,6 @@
+['ant', 'bear', 'caterpillar'].pop().length; // 2
+
+// The pop method mutates the caller array by removing
+// the last element from it and returns. The length
+// property then returns the length of the returned
+// string 'caterpillar' which is 11.
