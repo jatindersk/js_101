8866dbe91becf4aae23fcc5a2b1b85f050c424ea
Jatinder Koranga <jatinderskk8@gmail.com>
2022-01-03 05:19:05 +0530

    ex 8 and 9

---
+++ js101_small_problems/medium_1/ex7.js
@@ -0,0 +1,14 @@
+function fibonacci(nth) {
+  let previousTwo = [1, 1];
+
+  for (let counter = 3; counter <= nth; counter += 1) {
+    previousTwo = [previousTwo[1], previousTwo[0] + previousTwo[1]];
+    console.log(`${previousTwo} ${counter}`);
+  }
+
+  return previousTwo[1];
+}
+
+console.log(fibonacci(20));       // 6765
+console.log(fibonacci(50));       // 12586269025
+console.log(fibonacci(75));       // 2111485077978050
---
+++ js101_small_problems/medium_1/ex8.js
@@ -0,0 +1,19 @@
+let obj = {};
+let fibonacci = nth => {
+  if (nth <= 2) {
+    return 1;
+  } else if (obj[nth]) {
+    return obj[nth];
+  } else {
+    obj[nth] = fibonacci(nth - 1) + fibonacci(nth - 2);
+    return obj[nth];
+  }
+};
+
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765
