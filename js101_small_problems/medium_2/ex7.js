144d96f4c764b02edd365c036c005709c3773f27
Jatinder Koranga <jatinderskk8@gmail.com>
2022-01-04 10:59:11 +0530

    ex 7

---
+++ js101_small_problems/medium_2/ex7.js
@@ -0,0 +1,25 @@
+let bubbleSort = arr => {
+  while (true) {
+    let swap = false;
+    for (let idx = 0; idx < arr.length; idx += 1) {
+      if (arr[idx] > arr[idx + 1]) {
+        [arr[idx], arr[idx + 1]] = [arr[idx + 1], arr[idx]];
+        swap = true;
+      }
+    }
+    if (!swap) break;
+  }
+};
+
+
+let array1 = [5, 3];
+bubbleSort(array1);
+console.log(array1);    // [3, 5]
+
+let array2 = [6, 2, 7, 1, 4];
+bubbleSort(array2);
+console.log(array2);    // [1, 2, 4, 6, 7]
+
+let array3 = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie'];
+bubbleSort(array3);
+console.log(array3);    // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
