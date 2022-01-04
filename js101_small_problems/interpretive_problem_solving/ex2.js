2af6bc9b2692b5686277d8f44ef853ce5474eeb4
Jatinder Koranga <jatinderskk8@gmail.com>
2022-01-04 22:43:30 +0530

    ex2

---
+++ js101_small_problems/interpretive_problem_solving/ex2.js
@@ -0,0 +1,43 @@
+function diamond(n) {
+  numberSequence(n).forEach(number => {
+    console.log(`${" ".repeat((n - number) / 2)}${"*".repeat(number)}`);
+  });
+}
+
+function numberSequence(n) {
+  let result = [];
+  let increment = 2;
+  let number = 1;
+
+  while (number > 0) {
+    result.push(number);
+    if (number === n) {
+      increment = -2;
+    }
+    number += increment;
+  }
+
+  return result;
+}
+
+diamond(1);
+// logs
+// *
+
+diamond(3);
+// logs
+//  *
+// ***
+//  *
+
+diamond(9);
+// logs
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
