fbe8c7903b11df75e1b68ac34802ecd2cd5f14a8
Jatinder Koranga <jatinderskk8@gmail.com>
2022-01-28 23:45:21 +0530

    medium 1

---
+++ practice/small_problems_1/ex11.js
@@ -0,0 +1,12 @@
+// algorithm:
+//  - every number is the sum of previous two numbers
+//  - f(1) and f(2) is fixed i.e 1
+//  - 
+
+let fibonacci = n => {
+
+};
+
+fibonacci(20);       // 6765
+fibonacci(50);       // 12586269025
+fibonacci(75);       // 2111485077978050
