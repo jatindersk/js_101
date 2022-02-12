5e36e250057f55821df90539215a34c967b6c393
Jatinder Koranga <jatinderskk8@gmail.com>
2022-02-12 17:57:07 +0530

    interpretive ex2

--- practice/small_problems_1/ex51.js
+++ practice/small_problems_1/ex51.js
@@ -1,14 +1,46 @@
-function randomNumberBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+//  input: odd number, output: pattern of * (a string)
+//  rules:
+//  - the pattern starts by displaying one star
+//  - it then increments by 2 {1, 3, ... upto n (input number)}
+//  - the number of space characters prepending the star is 
+//  initially equal to the (input number) / 2
+//  - the space then decreases by a count of 1
+//  algorithm:
+//  - define a loop that will iterate from 0 to the input number / 2 - 1
+//  - on each iteration it will output the star and prepend space to them
 
-let tries = 0;
-let result = randomNumberBetween(1, 6);
-tries += 1;
+let diamond = num => {
+  let midPoint = Math.floor(num / 2);
 
-while (result <= 2) {
-  result = randomNumberBetween(1, 6);
-  tries += 1;
-}
+  for (let idx = 0; idx < midPoint; idx += 1) {
+    console.log(`${' '.repeat(midPoint - idx)}${'*'.repeat((idx * 2) + 1)}`);
+  }
 
-console.log('It took ' + String(tries) + ' tries to get a nu`mber greater than 2');
+  console.log('*'.repeat(num));
+
+  for (let idx = midPoint - 1; idx >= 0; idx -= 1) {
+    console.log(`${' '.repeat(midPoint - idx)}${'*'.repeat((idx * 2) + 1)}`);
+  }
+};
+
+diamond(3);
+// logs
+//  *
+// ***
+//  *
+
+diamond(1);
+// logs
+// *
+
+diamond(9);
+// logs
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
