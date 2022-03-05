b545c5f21a53ce45c9025e4405e530e110ac6239
Jatinder Koranga <jatinderskk8@gmail.com>
2022-03-05 16:50:31 +0530

    ex

---
+++ practice/small_problems_1/ex74.js
@@ -0,0 +1,16 @@
+let wordSizes = str => {
+  let result = {};
+  if (str.length === 0) return result;
+
+  str.split(' ').forEach(word => {
+    let length = word.length;
+    result[length] = result[length] || 0;
+    result[length] += 1;
+  });
+  return result;
+};
+
+console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
+console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 1, "7": 2 }
+console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
+console.log(wordSizes(''));                                            // {}
