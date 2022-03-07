6db7a6b10b150a0be8b534b1fe3a93106dbfbf29
Jatinder Koranga <jatinderskk8@gmail.com>
2022-03-07 21:28:53 +0530

    ex

---
+++ practice/small_problems_1/ex89.js
@@ -0,0 +1,5 @@
+
+
+console.log(leadingSubstrings('abc'));      // ["a", "ab", "abc"]
+console.log(leadingSubstrings('a'));        // ["a"]
+console.log(leadingSubstrings('xyzzy'));    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
