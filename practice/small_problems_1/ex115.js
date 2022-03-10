ed8deacf9bc88001f7a314cff26d224ceb19937c
Jatinder Koranga <jatinderskk8@gmail.com>
2022-03-10 21:10:53 +0530

    ex

---
+++ practice/small_problems_1/ex115.js
@@ -0,0 +1,79 @@
+//  split the string into sentences
+//  iterate through the string and look for (?, ! or .) characters
+//  if found a matching character :
+//  - slice the string upto the matching character
+//  - push the sliced substring into an array
+//  find the number of words in each sentence
+
+let longestSentence = str => {
+  let sentences = [];
+  let wordsArr = str.split(' ');
+  let startIdx = 0;
+
+  for (let idx = 0; idx < wordsArr.length; idx += 1) {
+    let word = wordsArr[idx];
+
+    if ((`.!?`.includes(word[word.length - 1]))) {
+      let endIdx = idx + 1;
+      sentences.push(wordsArr.slice(startIdx, endIdx));
+      startIdx = idx + 1;
+    }
+  }
+
+  let longestSentence = findLongest(sentences);
+
+  console.log(`${longestSentence.join(' ')}`);
+  console.log(`The longest sentence has ${longestSentence.length} words.`);
+};
+
+function findLongest(sentences) {
+  sentences.sort((a, b) => a.length - b.length);
+  return sentences[sentences.length - 1];
+}
+
+let longText =
+  'Four score and seven years ago our fathers brought forth on this ' +
+  'continent a new nation, conceived in liberty, and dedicated to the ' +
+  'proposition that all men are created equal. Now we are engaged in a ' +
+  'great civil war, testing whether that nation, or any nation so ' +
+  'conceived and so dedicated, can long endure. We are met on a great ' +
+  'battlefield of that war. We have come to dedicate a portion of that ' +
+  'field, as a final resting place for those who here gave their lives ' +
+  'that that nation might live. It is altogether fitting and proper that ' +
+  'we should do this.';
+
+let longerText = longText +
+  'But, in a larger sense, we can not dedicate, we can not consecrate, ' +
+  'we can not hallow this ground. The brave men, living and dead, who ' +
+  'struggled here, have consecrated it, far above our poor power to add ' +
+  'or detract. The world will little note, nor long remember what we say ' +
+  'here but it can never forget what they did here. It is for us the ' +
+  'living, rather, to be dedicated here to the unfinished work which ' +
+  'they who fought here have thus far so nobly advanced. It is rather ' +
+  'for us to be here dedicated to the great task remaining before us -- ' +
+  'that from these honored dead we take increased devotion to that ' +
+  'cause for which they gave the last full measure of devotion -- that ' +
+  'we here highly resolve that these dead shall not have died in vain ' +
+  '-- that this nation, under God, shall have a new birth of freedom -- ' +
+  'and that government of the people, by the people, for the people, ' +
+  'shall not perish from the earth.';
+
+longestSentence(longText);
+// Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.
+//
+// The longest sentence has 30 words.
+
+longestSentence(longerText);
+// It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth.
+//
+// The longest sentence has 86 words.
+
+longestSentence("Where do you think you're going? What's up, Doc?");
+// Where do you think you're going?
+//
+// The longest sentence has 6 words.
+
+longestSentence("To be or not to be! Is that the question?");
+// To be or not to be!
+//
+// The longest sentence has 6 words.
