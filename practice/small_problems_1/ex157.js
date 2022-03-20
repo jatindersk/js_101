c128407d4aa871d7f21b96d53ae4f4064568eeb1
Jatinder Koranga <jatinderskk8@gmail.com>
2022-03-20 08:24:11 +0530

    codewars LS

--- practice/small_problems_1/ex157.js
+++ practice/small_problems_1/ex157.js
@@ -1,3 +1,11 @@
-function domainName(url){
-  //your code here
+function domainName(url) {
+  if (url.includes('www')) return url.split('.')[1];
+  else if (url.includes('//')) return url.split('//')[1].split('.')[0];
+
+  return url.split('.')[0];
 }
+
+console.log(domainName("http://github.com/carbonfive/raygun")); // == "github"
+console.log(domainName("http://www.zombie-bites.com")); // == "zombie-bites"
+console.log(domainName("https://www.cnet.com")); // == "cnet"
+console.log(domainName("desk-wt2r6.co/")); // == "desk-wt2r6"
